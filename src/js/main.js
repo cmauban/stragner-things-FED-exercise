5e3d58dd434d2c9405cb36a06e78c0c2c176eea2
Camille Mauban <camille.mauban@gmail.com>
2019-07-09 16:10:17 -0400

    data from json coming through

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,15 @@
 //IIFE for Angular module
 ;(function(){
 
-  var app = angular.module('', []);
+  var app = angular.module('strangerThings', []);
 
-    app.controller('', function($http, $rootScope){
+    app.controller('stController', function($http, $rootScope){
 
-      $http.get('')
+      $http.get('../api/en_US.json')
         .then(function(response){
           $rootScope.items = response.data;
+          /* check in console */
+          console.log(response.data);
         });
 
     }); // end countController
