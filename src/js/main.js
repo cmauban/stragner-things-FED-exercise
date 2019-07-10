e5a1b3abf1169857c5a49ee29969f8bc19fae3b7
Camille Mauban <camille.mauban@gmail.com>
2019-07-10 16:39:42 -0400

    gallery with data in place

--- src/js/main.js
+++ src/js/main.js
@@ -10,10 +10,25 @@
           $rootScope.items = response.data;
           /* check in console */
           console.log(response.data);
-
+          var english = response.data;
           // items.locations.toString() = location
         });
+
+      /* get pig latin data */
+      $http.get('../api/la_PG.json')
+        .then(function(response){
+          $rootScope.plitems = response.data;
+          console.log(response.data);
+          var pigLatin = response.data;
+        });
+
+      // TODO: get translated content working.
+      // reference: https://docs.angularjs.org/api/ng/directive/select
+
+      var selectedLang = this;
 
     }); // end stController
 
 })(); // end iffy
+
+// Gallery
